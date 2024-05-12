717a779ecbe01778ce11127fe6dbb9d3e18805a3
Georgiana-Mădălina Dragomir <dragomirgeorgianamadalina@gmail.com>
2024-05-13 00:32:34 +0300

    Worked on first task

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,53 @@
-export const App = () => {
+// App.jsx
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from 'react-router-dom';
+import Home from './components/Home';
+import Movies from './components/Movies';
+import MovieDetails from './components/MovieDetails';
+import Cast from './components/Cast';
+import Reviews from './components/Reviews';
+
+// Import the API key
+import config from './config';
+
+function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        {/* Pass the API key as a prop to Movies component */}
+        <Route
+          exact
+          path="/movies"
+          render={() => <Movies apiKey={config.API_KEY} />}
+        />
+        {/* Pass the API key as a prop to MovieDetails component */}
+        <Route
+          exact
+          path="/movies/:movieId"
+          render={props => <MovieDetails {...props} apiKey={config.API_KEY} />}
+        />
+        {/* Pass the API key as a prop to Cast component */}
+        <Route
+          exact
+          path="/movies/:movieId/cast"
+          render={props => <Cast {...props} apiKey={config.API_KEY} />}
+        />
+        {/* Pass the API key as a prop to Reviews component */}
+        <Route
+          exact
+          path="/movies/:movieId/reviews"
+          render={props => <Reviews {...props} apiKey={config.API_KEY} />}
+        />
+        <Redirect to="/" />
+      </Switch>
+    </Router>
   );
-};
+}
+
+export default App;
---
+++ src/components/config.js
@@ -0,0 +1,5 @@
+const config = {
+  API_KEY: '50b20c66c50bad6485195116f9f5592a',
+};
+
+export default config;
